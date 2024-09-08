0790efef126bd7300996705047e4189585c8974a
RafaelOBrasil <rafael-lunia@hotmail.com>
2024-09-08 18:33:06 -0300

    Att. Criação do Banco

--- db/init_db.py
+++ db/init_db.py
@@ -1,28 +1,31 @@
 import sqlite3
 
-con = sqlite3.connect("./db/to_do_list.db")
 
-cur = con.cursor()
+class CriarDB:
+    def novoDB():
+        con = sqlite3.connect("./db/to_do_list.db")
 
-cur.execute("""
-    CREATE TABLE IF NOT EXISTS USER (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome TEXT NOT NULL,
-        senha TEXT NOT NULL
-    )
-""")
+        cur = con.cursor()
 
-cur.execute("""
-    CREATE TABLE IF NOT EXISTS TAREFAS (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        titulo TEXT NOT NULL,
-        descricao TEXT,
-        data TEXT,
-        status TEXT,
-        id_usuario INTEGER,
-        FOREIGN KEY (id_usuario) REFERENCES USER(id)
-    )
-""")
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS USER (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome TEXT NOT NULL,
+                senha TEXT NOT NULL
+            )
+        """)
 
-con.commit()
-con.close()
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS TAREFAS (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                titulo TEXT NOT NULL,
+                descricao TEXT,
+                data TEXT,
+                status TEXT,
+                id_usuario INTEGER,
+                FOREIGN KEY (id_usuario) REFERENCES USER(id)
+            )
+        """)
+
+        con.commit()
+        con.close()
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+from db.init_db import CriarDB
 from views import *
 
 if __name__ == "__main__":
+    CriarDB.novoDB()
     app.run(debug=True)
 
